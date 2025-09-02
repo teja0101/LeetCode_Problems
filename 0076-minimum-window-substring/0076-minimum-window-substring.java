d949c90dc50d4ad9aae48826c54a0ee56981f62d
LAKSHMIPURAM TEJA <140742318+teja0101@users.noreply.github.com>
2025-09-02 17:52:24 +0530

    Time: 23 ms (25.43%), Space: 45.8 MB (15.25%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,43 +1,46 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int p1 = 0, p2 = 0, slen = s.length(), len = Integer.MAX_VALUE;
+        int sLen = s.length(), len = Integer.MAX_VALUE;
         int[] ans = new int[2];
-
         Map<Character, Integer> map = new HashMap<>();
-        for (int idx = 0; idx < t.length(); idx++) {
-            char ele = t.charAt(idx);
-            map.put(ele, map.getOrDefault(ele, 0) + 1);
-        }
         Map<Character, Integer> wid = new HashMap<>();
-        while (p2 < slen) {
-            char ele = s.charAt(p2);
+
+        for (int i = 0; i < t.length(); i++) {
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        int need = map.size(), have = 0, l = 0;
+
+        for (int r = 0; r < sLen; r++) {
+            char ele = s.charAt(r);
             wid.put(ele, wid.getOrDefault(ele, 0) + 1);
 
-            while (isMap1GreaterOrEqual(wid, map)){
-                if (len > (p2 - p1  + 1)) {
-                    len = p2 - p1 + 1;
-                    ans[0] = p1;
-                    ans[1] = p2 + 1;
-                }
-                ele = s.charAt(p1);
-                wid.put(ele,wid.get(ele) - 1);
-                if((wid.get(ele)) == 0)
-                    wid.remove(ele);
-                
-                p1++;
-
+            if (map.containsKey(ele) && wid.get(ele).intValue() == map.get(ele).intValue()) {
+                have++;
             }
-         p2++;
 
+            while (have == need) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ans[0] = l;
+                    ans[1] = r + 1;
+                }
+
+                ele = s.charAt(l);
+                wid.put(ele, wid.get(ele) - 1);
+                if (wid.get(ele) == 0) {
+                    wid.remove(ele);
+                }
+
+                if (map.containsKey(ele) && wid.getOrDefault(ele, 0) < map.get(ele)) {
+                    have--;
+                }
+
+                l++;
+            }
         }
-        if(len == Integer.MAX_VALUE)return"";
-        
-        String str = s.substring(ans[0], ans[1]);
-        return str;
-    }
 
-    static boolean isMap1GreaterOrEqual(Map<Character, Integer> map1, Map<Character, Integer> map2) {
-        return map2.entrySet().stream()
-                .allMatch(e -> map1.containsKey(e.getKey()) && map1.get(e.getKey()) >= e.getValue());
+        if(len == Integer.MAX_VALUE) return "";
+        return s.substring(ans[0], ans[1]);
     }
 }
